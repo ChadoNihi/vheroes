ec35149e3604269cdd831774aae77b84db4ef027
ChadoNihi <chadoishere@gmail.com>
2016-12-01 14:12:14 +0200

    save

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,19 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
-const rootReducer = ()=> {};
+import {} from "./actions/actionTypes.js";
+
+const initialState = {
+  heroInFocus: 0,
+  sortBy: 'id',
+  heroes
+};
+
+const rootReducer = (state = initialState, action = {})=> {
+  switch (action.type) {
+    default:
+      return state;
+};
 
 export default combineReducers({
   rootReducer,
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import reducer from '../reducer';
 
-export default function configureStore(initialState) {
+export default function configureStore() {
   return createStore(
     reducer,
     //applyMiddleware(thunkMiddleware)
