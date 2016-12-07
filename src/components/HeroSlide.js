9d5d4c31eba9eeab5a1c1b2c7a92d9bb52cd87ff
ChadoNihi <chadoishere@gmail.com>
2016-12-07 23:57:46 +0200

    save

--- src/components/HeroSlide.js
+++ src/components/HeroSlide.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default ({imgSrc, name, shortIntro, what, see})=> {
   return (
     <div className='hero-block'>
-      <img src={imgSrc} alt={`most probably a photo of ${name}`} />
-      <h2>{name}</h2>
-      <p>{shortIntro}</p>
+      <img className='hero-img' src={imgSrc} alt={`most probably a photo of ${name}`} />
+      <h1 className='hero-name'>{name}</h1>
+      <h6>{shortIntro}</h6>
       <p>{what}</p>
     </div>
   );
