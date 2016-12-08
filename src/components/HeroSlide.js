8284c9357968bb214433c3bdf8aab37b263b93a8
ChadoNihi <chadoishere@gmail.com>
2016-12-08 15:47:05 +0200

    save

--- src/components/HeroSlide.js
+++ src/components/HeroSlide.js
@@ -4,9 +4,19 @@ export default ({imgSrc, name, shortIntro, what, see})=> {
   return (
     <div className='hero-block'>
       <img className='hero-img' src={imgSrc} alt={`most probably a photo of ${name}`} />
-      <h1 className='hero-name'>{name}</h1>
-      <h6>{shortIntro}</h6>
-      <p>{what}</p>
+      <h1 className='hero-name' dangerouslySetInnerHTML={{__html: name}}></h1>
+      <h6 dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
+      <p dangerouslySetInnerHTML={{__html: what}}></p>
+      
+      <ul className="mdl-list">
+        {see.map((opt, i)=> {
+          return (
+            <li className="mdl-list__item" key={i}>
+              <span className="mdl-list__item-primary-content" dangerouslySetInnerHTML={{__html: opt}}></span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
