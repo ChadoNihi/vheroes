37b9f60263f8183c8a7651f9b1ad50c5921b8b21
ChadoNihi <chadoishere@gmail.com>
2016-12-09 13:30:34 +0200

    messing with style

--- src/components/HeroSlide.js
+++ src/components/HeroSlide.js
@@ -5,8 +5,8 @@ export default ({imgSrc, name, shortIntro, what, see})=> {
     <div className='hero-block'>
       <img className='hero-img' src={imgSrc} alt={`most probably a photo of ${name}`} />
       <h1 className='hero-name' dangerouslySetInnerHTML={{__html: name}}></h1>
-      <h6 className='save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
-      <p className='save-spaces' dangerouslySetInnerHTML={{__html: what}}></p>
+      <h6 className='hero-intro save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
+      <p className='hero-what save-spaces' dangerouslySetInnerHTML={{__html: what}}></p>
 
       <h4>Explore:</h4>
       <ul className="mdl-list">
