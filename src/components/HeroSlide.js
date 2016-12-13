a31fe3a5e935e87f4acff87fd09bfe38ef497a4d
ChadoNihi <chadoishere@gmail.com>
2016-12-13 22:41:58 +0200

    save

--- src/components/HeroSlide.js
+++ src/components/HeroSlide.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export default ({imgSrcs, name, shortIntro, what, see})=> {
+export default ({imgSrcs, name, shortIntro, what, quotes, see})=> {
   return (
     <div className='hero-block'>
       <img className='hero-img' src={imgSrcs.default} alt={`most probably a photo of ${name}`} />
       <h1 className='hero-name' dangerouslySetInnerHTML={{__html: name}}></h1>
       <h6 className='hero-intro save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
       <p className='hero-what save-spaces' dangerouslySetInnerHTML={{__html: what}}></p>
+      {quotes.length && quotes.map((quote, i)=> {
+        return <div className='hero-quote' key={i}>{quote}</div>;
+      })}
 
       <h4>Explore:</h4>
       <ul className="mdl-list">
