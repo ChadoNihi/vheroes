b320670fc28fd6ec0b1e8f4bbd439d58efef5374
ChadoNihi <chadoishere@gmail.com>
2016-12-14 14:09:28 +0200

    save

--- src/components/HeroSlide.js
+++ src/components/HeroSlide.js
@@ -2,50 +2,47 @@ import React from 'react';
 
 export default ({imgSrcs, name, shortIntro, what, quotes, see})=> {
   return (
-    <div className='hero-block'>
-      <img className='hero-img' src={imgSrcs.default} alt={`most probably a photo of ${name}`} />
-      <h1 className='hero-name'>{name}</h1>
-      <h6 className='hero-intro save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
-      <p className='hero-what save-spaces' dangerouslySetInnerHTML={{__html: what}}></p>
+    <div className="mdl-card hero-card">
+      <div className="mdl-card__title">
+        <h1 className='hero-name'>{name}</h1>
+        <h6 className='hero-intro save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
+      </div>
+      <div className="mdl-card__media">
+        <img className='hero-img' src={imgSrcs.default} alt={`most probably a photo of ${name}`} />
+      </div>
+      <div className="hero-what mdl-card__supporting-text" dangerouslySetInnerHTML={{__html: what}}>
+      </div>
       {quotes.length && quotes.map((quote, i)=> {
-        return <div className='hero-quote' key={i}>{quote}</div>;
+        return <div className='hero-quote mdl-card__supporting-text' key={i}>{quote}</div>;
       })}
-
-      <h4>Explore:</h4>
-      <ul className="mdl-list">
+      <div className="mdl-card__actions">
         {see.map((opt, i)=> {
           return (
-            <li className="mdl-list__item explore-list__item" key={i}>
-              <span className="mdl-list__item-primary-content save-spaces" dangerouslySetInnerHTML={{__html: opt}}></span>
-            </li>
+            <span className="mdl-list__item-primary-content save-spaces" dangerouslySetInnerHTML={{__html: opt}} key={i}></span>
           );
-        })}
-      </ul>
-
-      <div className="mdl-card">
-        <div className="mdl-card__title">
-          <h1 className='hero-name'>{name}</h1>
-          <h6 className='hero-intro save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
-        </div>
-        <div className="mdl-card__media">
-          <img className='hero-img' src={imgSrcs.default} alt={`most probably a photo of ${name}`} />
-        </div>
-        <div className="hero-what mdl-card__supporting-text" dangerouslySetInnerHTML={{__html: what}}>
-        </div>
-        {quotes.length && quotes.map((quote, i)=> {
-          return <div className='hero-quote mdl-card__supporting-text' key={i}>{quote}</div>;
-        })}
-        <div className="mdl-card__actions">
-          {see.map((opt, i)=> {
-            return (
-              <li className="mdl-list__item explore-list__item" key={i}>
-                <span className="mdl-list__item-primary-content save-spaces" dangerouslySetInnerHTML={{__html: opt}}></span>
-              </li>
-            );
-        })}
-        </div>
+      })}
       </div>
-
     </div>
   );
 };
+
+// <div className='hero-block'>
+//   <img className='hero-img' src={imgSrcs.default} alt={`most probably a photo of ${name}`} />
+//   <h1 className='hero-name'>{name}</h1>
+//   <h6 className='hero-intro save-spaces' dangerouslySetInnerHTML={{__html: shortIntro}}></h6>
+//   <p className='hero-what save-spaces' dangerouslySetInnerHTML={{__html: what}}></p>
+//   {quotes.length && quotes.map((quote, i)=> {
+//     return <div className='hero-quote' key={i}>{quote}</div>;
+//   })}
+//
+//   <h4>Explore:</h4>
+//   <ul className="mdl-list">
+//     {see.map((opt, i)=> {
+//       return (
+//         <li className="mdl-list__item explore-list__item" key={i}>
+//           <span className="mdl-list__item-primary-content save-spaces" dangerouslySetInnerHTML={{__html: opt}}></span>
+//         </li>
+//       );
+//     })}
+//   </ul>
+// </div>
