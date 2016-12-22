37fc1a438ce76bafe395fcc6fb4d0a6b717c8887
ChadoNihi <chadoishere@gmail.com>
2016-12-22 21:18:29 +0200

    save

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,20 @@ export default ()=> {
   return (
     <div className="mdl-card hero-card mdl-shadow--16dp">
       <div className="mdl-card__title hero-card-title">
-        <h2 className='hero-name'>Expand your think space</h2>
+        <h2 className='mdl-card__title-text'>Expand your think space</h2>
+        <h3 className='mdl-card__subtitle-text'>Consider others</h3>
       </div>
       <div className="hero-what mdl-card__supporting-text">
         Irrespective of what you thinks about this website, you should definetely familiarize oneself with the work of exceptional persons presented here.
-
-        You don't need to be a vegan to enjoy the content.
+      </div>
+      <div className="hero-what mdl-card__supporting-text">
+        A diverse set of philosophers, scientists, activists, writers, artists, and others with all sorts of professional backgrounds...
+      </div>
+      <div className="hero-what mdl-card__supporting-text">
+        You don't need to be a vegan to enjoy the content (the funny thing BTW, one doesn't need to be a vegan <em>personally</em> to end up the list.)
       </div>
       <div className="mdl-card__actions">
-        <a class="mdl-button mdl-js-button mdl-js-ripple-effect">
+        <a class="mdl-button mdl-js-button mdl-js-ripple-effect" href='#'>
           Contact
         </a>
       </div>
