11ced8e7b958ac61c5011f823440618943a61a41
ChadoNihi <chadoishere@gmail.com>
2016-12-26 22:01:08 +0200

    save

--- src/components/ToTheTopBtn.js
+++ src/components/ToTheTopBtn.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ()=> {
   return (
     <a href='#' id='to-the-top-btn' className="mdl-button mdl-js-button">
-      <i className="fa fa-angle-double-up" aria-hidden="false"></i>
+      <i className="fa fa-angle-double-up fa-4x" aria-hidden="false"></i>
     </a>
   );
 }
