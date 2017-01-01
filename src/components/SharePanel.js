7b389a86478d9e27cacdebcbd073a59901327c04
ChadoNihi <chadoishere@gmail.com>
2017-01-01 21:57:02 +0200

    save

---
+++ src/components/SharePanel.js
@@ -0,0 +1,13 @@
+import {
+  ShareButtons,
+  generateShareIcon
+} from 'react-share';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  VKShareButton
+} = ShareButtons;
