45057b71d92e4449312f00aa0e35256896cdd832
ChadoNihi <chadoishere@gmail.com>
2017-01-21 00:14:54 +0200

    save

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://bright-vegans.herokuapp.com';
+export const baseUrl = 'https://vegan-heroes.herokuapp.com';
 export const reverseSortSuffix = '-alt';
 export const siteName = "Considerals" // "Vegan Heroes";
